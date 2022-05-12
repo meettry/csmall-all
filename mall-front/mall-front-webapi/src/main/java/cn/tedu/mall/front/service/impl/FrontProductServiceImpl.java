a096c5bd2f62838575cede9a42115485c2e38b9f
Xiloer <83123095@qq.com>
2022-05-12 16:06:09 +0800

    Spu业务逻辑层更新

--- mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/service/impl/FrontProductServiceImpl.java
+++ mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/service/impl/FrontProductServiceImpl.java
@@ -3,6 +3,7 @@ package cn.tedu.mall.front.service.impl;
 import cn.tedu.mall.common.restful.JsonPage;
 import cn.tedu.mall.front.service.IFrontProductService;
 import cn.tedu.mall.pojo.product.vo.*;
+import cn.tedu.mall.product.service.front.IForFrontSkuService;
 import cn.tedu.mall.product.service.front.IForFrontSpuService;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,8 @@ public class FrontProductServiceImpl implements IFrontProductService {
 
     @DubboReference
     private IForFrontSpuService dubboSpuService;
-
+    @DubboReference
+    private IForFrontSkuService dubboSkuService;
 
     @Override
     public JsonPage<SpuListItemVO> listSpuByCategoryId(Long categoryId, Integer page, Integer pageSize) {
@@ -25,19 +27,29 @@ public class FrontProductServiceImpl implements IFrontProductService {
         return spuListItemVOJsonPage;
     }
 
+    // 根据spuid 查询spu对象
     @Override
     public SpuStandardVO getFrontSpuById(Long id) {
-        return null;
+        // 调用根据id查询spu的product方法
+        SpuStandardVO spuStandardVO=dubboSpuService.getSpuById(id);
+        // 别忘了返回spuStandardVO
+        return spuStandardVO;
     }
 
+    // 根据spuId查询 当前商品所有sku列表
     @Override
     public List<SkuStandardVO> getFrontSkusBySpuId(Long spuId) {
-        return null;
+        List<SkuStandardVO> list=dubboSkuService.getSkusBySpuId(spuId);
+        // 别忘了返回
+        return list;
     }
 
+    // 根据spuid查询 spu的详情
     @Override
     public SpuDetailStandardVO getSpuDetail(Long spuId) {
-        return null;
+        SpuDetailStandardVO spuDetailStandardVO=dubboSpuService.getSpuDetailById(spuId);
+        // 别忘了返回spuDetailStandardVO
+        return spuDetailStandardVO;
     }
 
     @Override
