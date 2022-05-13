fcfe602681f90c0a89ddd326cac771defe94a4b0
Xiloer <83123095@qq.com>
2022-05-13 15:02:26 +0800

    新增购物车业务逻辑层

--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsCartMapper.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsCartMapper.java
@@ -1,4 +1,20 @@
 package cn.tedu.mall.order.mapper;
 
+import cn.tedu.mall.pojo.order.model.OmsCart;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface OmsCartMapper {
+
+    // 1.判断当前登录用户的购物车中是否包含指定sku的方法
+    OmsCart selectExistCart(@Param("userId")Long userId,@Param("skuId")Long skuId);
+
+    // 2.保存到购物车的方法
+    void saveCart(OmsCart omsCart);
+
+    // 3.修改指定购物车信息数量的方法
+    void updateQuantityById(OmsCart omsCart);
+
+
 }
--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/service/impl/OmsCartServiceImpl.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/service/impl/OmsCartServiceImpl.java
@@ -1,4 +1,71 @@
 package cn.tedu.mall.order.service.impl;
 
-public class OmsCartServiceImpl {
+import cn.tedu.mall.common.exception.CoolSharkServiceException;
+import cn.tedu.mall.common.pojo.domain.CsmallAuthenticationInfo;
+import cn.tedu.mall.common.restful.JsonPage;
+import cn.tedu.mall.common.restful.ResponseCode;
+import cn.tedu.mall.order.service.IOmsCartService;
+import cn.tedu.mall.pojo.order.dto.CartAddDTO;
+import cn.tedu.mall.pojo.order.dto.CartUpdateDTO;
+import cn.tedu.mall.pojo.order.model.OmsCart;
+import cn.tedu.mall.pojo.order.vo.CartStandardVO;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OmsCartServiceImpl implements IOmsCartService {
+    @Override
+    public void addCart(CartAddDTO cartDTO) {
+
+    }
+
+    @Override
+    public JsonPage<CartStandardVO> listCarts(Integer page, Integer pageSize) {
+        return null;
+    }
+
+    @Override
+    public void removeCart(Long[] ids) {
+
+    }
+
+    @Override
+    public void removeAllCarts() {
+
+    }
+
+    @Override
+    public void removeUserCarts(OmsCart omsCart) {
+
+    }
+
+    @Override
+    public void updateQuantity(CartUpdateDTO cartUpdateDTO) {
+
+    }
+
+    // 业务逻辑层中可能有多个方法需要获得当前用户信息
+    // 我们可以定义一个方法实现从SpringSecurity中获得用户信息
+    public CsmallAuthenticationInfo getUserInfo(){
+        // 从SpringSecurity框架的容器中,获得当前用户的authenticationToken对象
+        UsernamePasswordAuthenticationToken authenticationToken=
+                (UsernamePasswordAuthenticationToken)SecurityContextHolder
+                                        .getContext().getAuthentication();
+        // 判断获取的对象是不是null
+        if(authenticationToken!=null){
+            // 如果不是空,就是登录成功了,从authenticationToken对象中获得当前登录用户
+            CsmallAuthenticationInfo csmallAuthenticationInfo=
+                    (CsmallAuthenticationInfo)authenticationToken.getCredentials();
+            return csmallAuthenticationInfo;
+        }
+        throw new CoolSharkServiceException(ResponseCode.UNAUTHORIZED,"没有登录信息");
+    }
+
+
+
+
+
+
+
 }
