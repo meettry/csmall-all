2d4b1dab1e281baaf5ae250971174279c3739910
Xiloer <83123095@qq.com>
2022-05-13 16:03:50 +0800

    新增购物车完成

--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/controller/OmsCartController.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/controller/OmsCartController.java
@@ -1,4 +1,46 @@
 package cn.tedu.mall.order.controller;
 
+import cn.tedu.mall.common.restful.JsonResult;
+import cn.tedu.mall.order.service.IOmsCartService;
+import cn.tedu.mall.pojo.order.dto.CartAddDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/oms/cart")
+@Api(tags="购物车功能")
 public class OmsCartController {
+    @Autowired
+    private IOmsCartService cartService;
+    @PostMapping("/add")
+    @ApiOperation("当前登录用户新增sku到购物车")
+    // 判断当前用户是否为ROLE_user(是否登录了)
+    @PreAuthorize("hasRole('ROLE_user')")
+    // @Validated启动控制器运行前对CartAddDTO对象的SpringValidation验证
+    public JsonResult addCart(@Validated CartAddDTO cartAddDTO){
+        cartService.addCart(cartAddDTO);
+        return JsonResult.ok("添加到购物车完成");
+    }
+    // leaf   passport    order
+    // sso:10002   order:10005
+    // 先运行10002的knife4j 访问前台用户登录 登录成功复制jwt
+    // 转到order10005,在全局设置中设置参数
+    // name:   Authorization
+    // value:  Beaer [复制的jwt]
+    // 然后要刷新10005的knife4j页面
+    // 就可以发送新增到购物车的请求了
+
 }
+
+
+
+
+
+
+
--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/service/impl/OmsCartServiceImpl.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/service/impl/OmsCartServiceImpl.java
@@ -4,20 +4,46 @@ import cn.tedu.mall.common.exception.CoolSharkServiceException;
 import cn.tedu.mall.common.pojo.domain.CsmallAuthenticationInfo;
 import cn.tedu.mall.common.restful.JsonPage;
 import cn.tedu.mall.common.restful.ResponseCode;
+import cn.tedu.mall.order.mapper.OmsCartMapper;
 import cn.tedu.mall.order.service.IOmsCartService;
 import cn.tedu.mall.pojo.order.dto.CartAddDTO;
 import cn.tedu.mall.pojo.order.dto.CartUpdateDTO;
 import cn.tedu.mall.pojo.order.model.OmsCart;
 import cn.tedu.mall.pojo.order.vo.CartStandardVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OmsCartServiceImpl implements IOmsCartService {
+
+    @Autowired
+    private OmsCartMapper omsCartMapper;
+
     @Override
     public void addCart(CartAddDTO cartDTO) {
+        // 获取当前登录用户的userId
+        Long userId=getUserId();
+        // 判断这个userId的用户是否已经将指定的sku添加到购物车
+        OmsCart omsCartExist= omsCartMapper.selectExistCart(userId,cartDTO.getSkuId());
+        if(omsCartExist!=null) {
+            // 如果sku已经存在于购物车中,就修改这个sku在购物车中的数量
+            omsCartExist.setQuantity(omsCartExist.getQuantity()+cartDTO.getQuantity());
+            // 执行数据库修改
+            omsCartMapper.updateQuantityById(omsCartExist);
+        }else {
+            // 如果sky没有存在于购物车中,就新增sku信息到购物车
+            OmsCart omsCartNew=new OmsCart();
+            // 将cartDTO对象中的同名属性赋值给 新创建的OmsCart对象
+            BeanUtils.copyProperties(cartDTO,omsCartNew);
+            // OmsCart对象中赋值当前登录用户id
+            omsCartNew.setUserId(userId);
+            // 新增新增
+            omsCartMapper.saveCart(omsCartNew);
 
+        }
     }
 
     @Override
@@ -61,7 +87,10 @@ public class OmsCartServiceImpl implements IOmsCartService {
         }
         throw new CoolSharkServiceException(ResponseCode.UNAUTHORIZED,"没有登录信息");
     }
-
+    // 单纯获得当前登录用户id的方法
+    public Long getUserId(){
+        return getUserInfo().getId();
+    }
 
 
 
