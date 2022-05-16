469cafb49caf4e5068993bbf4a057862f85f68d7
Xiloer <83123095@qq.com>
2022-05-16 16:51:33 +0800

    生成订单的控制层完成

---
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/controller/OmsOrderController.java
@@ -0,0 +1,40 @@
+package cn.tedu.mall.order.controller;
+
+import cn.tedu.mall.common.restful.JsonResult;
+import cn.tedu.mall.order.service.IOmsOrderService;
+import cn.tedu.mall.pojo.order.dto.OrderAddDTO;
+import cn.tedu.mall.pojo.order.vo.OrderAddVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/oms/order")
+@Api(tags="订单功能")
+public class OmsOrderController {
+
+    @Autowired
+    private IOmsOrderService orderService;
+
+    @PostMapping("/add")
+    @ApiOperation("生成订单的方法")
+    @PreAuthorize("hasRole('ROLE_user')")
+    public JsonResult<OrderAddVO> addOrder(@Validated OrderAddDTO orderAddDTO){
+        OrderAddVO orderAddVO=orderService.addOrder(orderAddDTO);
+        return JsonResult.ok(orderAddVO);
+    }
+
+
+
+}
+
+
+
+
+
+
