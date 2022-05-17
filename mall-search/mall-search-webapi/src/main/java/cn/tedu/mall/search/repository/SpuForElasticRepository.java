04e19af9e65df9231e7f17147cbde44db1298c03
Xiloer <83123095@qq.com>
2022-05-17 17:38:49 +0800

    编写查询es的数据访问层

--- mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
+++ mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
@@ -1,10 +1,25 @@
 package cn.tedu.mall.search.repository;
 
 import cn.tedu.mall.pojo.search.entity.SpuForElastic;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SpuForElasticRepository
         extends ElasticsearchRepository<SpuForElastic,Long> {
+
+    // 查询spu关键字段中包含"手机"的方法
+    @Query("{\n" +
+            "    \"bool\": {\n" +
+            "      \"should\": [\n" +
+            "        { \"match\": { \"name\": \"手机\"}},\n" +
+            "        { \"match\": { \"title\": \"手机\"}},\n" +
+            "        { \"match\": { \"description\": \"手机\"}},\n" +
+            "        { \"match\": { \"category_name\": \"手机\"}}\n" +
+            "        ]\n" +
+            "     }\n" +
+            "}")
+    Iterable<SpuForElastic> querySearch();
+
 }
