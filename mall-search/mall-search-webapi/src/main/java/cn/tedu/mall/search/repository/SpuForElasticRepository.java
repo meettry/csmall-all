dbb42fc4cde63940bbb955b8a1583fea870b490b
Xiloer <83123095@qq.com>
2022-05-18 09:49:28 +0800

    执行虚拟机ES的搜索持久层

--- mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
+++ mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
@@ -1,5 +1,6 @@
 package cn.tedu.mall.search.repository;
 
+import cn.tedu.mall.pojo.search.entity.SpuEntity;
 import cn.tedu.mall.pojo.search.entity.SpuForElastic;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SpuForElasticRepository
         extends ElasticsearchRepository<SpuForElastic,Long> {
+
 
     // 查询spu关键字段中包含"手机"的方法
     @Query("{\n" +
