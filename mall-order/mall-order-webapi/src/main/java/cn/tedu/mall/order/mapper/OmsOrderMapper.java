d811a09e98671c008981babd46b3fe6f84acdbd4
jack <jack@gmail.com>
2022-07-04 11:02:15 +0800

    查询订单sql

--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsOrderMapper.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsOrderMapper.java
@@ -1,11 +1,20 @@
 package cn.tedu.mall.order.mapper;
 
+import cn.tedu.mall.pojo.order.dto.OrderListTimeDTO;
 import cn.tedu.mall.pojo.order.model.OmsOrder;
+import cn.tedu.mall.pojo.order.vo.OrderListVO;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OmsOrderMapper {
     // 新增订单对象到数据库的方法
     void insertOrder(OmsOrder order);
+
+    // 查询当前用户指定时间范围内的所有订单
+    List<OrderListVO> selectOrdersBetweenTimes(OrderListTimeDTO orderListTimeDTO);
+
+
 
 }
