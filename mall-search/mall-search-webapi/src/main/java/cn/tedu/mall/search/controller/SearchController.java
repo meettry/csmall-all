c10a8d8e96b5821011cb26ae59ec40823e8c986e
jack <jack@gmail.com>
2022-07-05 10:57:23 +0800

    查询商品信息的业务逻辑层

---
+++ mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/controller/SearchController.java
@@ -0,0 +1,48 @@
+package cn.tedu.mall.search.controller;
+
+import cn.tedu.mall.common.restful.JsonPage;
+import cn.tedu.mall.common.restful.JsonResult;
+import cn.tedu.mall.pojo.search.entity.SpuForElastic;
+import cn.tedu.mall.search.service.ISearchService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+@Api(tags = "搜索模块")
+public class SearchController {
+    @Autowired
+    private ISearchService searchService;
+
+    // 搜索模块最主要的功能就是实现搜索
+    // 面对这种功能对应的控制器方法,可以不写任何路径
+    // 按下面@GetMapping注解表示当前控制路径为localhost:10008/search
+    @GetMapping
+    @ApiOperation("根据关键字查询ES中的信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(value = "搜索关键字",name = "keyword",dataType = "string",
+                                                    required = true),
+            @ApiImplicitParam(value = "页码",name = "page",dataType = "int",
+                    required = true),
+            @ApiImplicitParam(value = "每页条数",name = "pageSize",dataType = "int",
+                    required = true)
+    })
+    public JsonResult<JsonPage<SpuForElastic>> searchByKeyword(
+            String keyword,
+            @RequestParam(value = "page",defaultValue = "1")Integer page,
+            @RequestParam(value = "pageSize",defaultValue = "5") Integer pageSize
+            ){
+        JsonPage<SpuForElastic> list=searchService.search(keyword,page,pageSize);
+        return JsonResult.ok(list);
+
+    }
+
+}
