31249283c25314f81ac5b1144f89caead72f4fa8
jack <jack@gmail.com>
2022-07-06 14:59:55 +0800

    开发数据加载需要的持久层

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSpuMapper.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSpuMapper.java
@@ -3,6 +3,7 @@ package cn.tedu.mall.seckill.mapper;
 import cn.tedu.mall.pojo.seckill.model.SeckillSpu;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -10,5 +11,14 @@ public interface SeckillSpuMapper {
 
     // 查询秒杀商品列表
     List<SeckillSpu> findSeckillSpus();
+
+    // 根据给定的时间查询正在秒杀的商品
+    List<SeckillSpu> findSeckillSpusByTime(LocalDateTime time);
+
+    // 查询所有秒杀商品的SpuId数据
+    Long[] findAllSeckillSpuIds();
+
+
+
 
 }
