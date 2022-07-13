01d89df5e7389b47d4c5921638ef02a7828515eb
jack <jack@gmail.com>
2022-07-13 11:49:14 +0800

    查询语句更新

--- mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
+++ mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
@@ -1,5 +1,6 @@
 package cn.tedu.mall.search.repository;
 
+import cn.tedu.mall.pojo.search.entity.SpuEntity;
 import cn.tedu.mall.pojo.search.entity.SpuForElastic;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,8 +10,19 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SpuForElasticRepository extends
-                        ElasticsearchRepository<SpuForElastic,Long> {
+                        ElasticsearchRepository<SpuEntity,Long> {
 
+    // 所有更新数据的操作都由Logstash负责,这里无需编写数据更新代码
+
+    // 方法名查询
+    Page<SpuEntity> querySpuEntitiesBySearchTextMatches(
+                                String keyword,Pageable pageable);
+    // 或者调用查询语句的方法
+    @Query("{\"match\":{\"search_text\":{\"query\":\"?0\"}}}")
+    Page<SpuEntity> querySearchByText(String keyword,Pageable pageable);
+
+
+/*
     // 自定义查询方法
     Iterable<SpuForElastic> querySpuForElasticsByTitleMatches(String title);
 
@@ -37,6 +49,6 @@ public interface SpuForElasticRepository extends
             "     }\n" +
             "}")
     Page<SpuForElastic> querySearch(String keyword, Pageable pageable);
-
+*/
 
 }
